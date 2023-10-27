9a6623bc9d1d7db9365f65490da75a8861571d3c
Haleem-Rafat <96750484+Haleem-rafat@users.noreply.github.com>
2023-10-27 20:16:29 +0200

    add env in vercel

--- src/app/config/envConfig.ts
+++ src/app/config/envConfig.ts
@@ -2,16 +2,4 @@ const envConfigKeys = {
   base_url: import.meta.env.VITE_API_BASE,
 };
 
-Object.entries(envConfigKeys).forEach(([key, val]) => {
-  if (!val) {
-    if (!import.meta.env.PROD)
-      return console.error(
-        `${key} is not existed, please check the envirnmental variables to fix this error!`
-      );
-    return console.error(
-      'Some configurations are missing, please contact the developers to check!'
-    );
-  }
-});
-
 export default envConfigKeys;
